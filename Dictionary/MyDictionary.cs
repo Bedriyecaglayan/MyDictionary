5d27d4f1dcc815b8bd6be1c9a1507cbb619ec548
Lenovo-pc <bedis730@gmail.com>
2021-01-23 18:46:40 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary <TKey, TValue>
+    {
+
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] temp = keys;
+            TValue[] tempvalues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                keys[i] = temp[i];
+                values[i] = tempvalues[i];
+            }
+            for (int j = 0; j < temp.Length; j++)
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException("Anahtar değeri boş olamaz.");
+                }
+                else if (temp[j].Equals(key))
+                {
+                    throw new ArgumentException("Aynı anahtar değeri mevcut.");
+                }
+
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+    }
+    }
---
+++ Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <int, string> MyDictionary = new MyDictionary <int, string>();
+            MyDictionary.Add(125, "Matematik");
+            MyDictionary.Add(853, "İstatistik");
+            MyDictionary.Add(120, "Programlama");
+        }
+    }
+}
